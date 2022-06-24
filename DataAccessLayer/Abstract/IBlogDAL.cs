875c069b86dc62ff6b0741aa3b69550195331d81
Emre <Emre@teknohub.com.tr>
2022-06-24 15:38:19 +0300

    Part-1

---
+++ DataAccessLayer/Abstract/IBlogDAL.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IBlogDAL
+    {
+        ICollection<Blog> ListAllCategory();
+        void AddBlog(Blog category);
+        void RemoveBlog(Blog category);
+        void UpdateBlog(Blog category);
+        Blog GetBlog(int id);
+    }
+}
